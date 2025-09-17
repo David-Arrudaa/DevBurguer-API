33302e8ee42ff12bd8070670a6742dc8a85455ae
David_Arruda <david.viniciusarruda@gmail.com>
2025-09-16 23:53:05 -0300

    Organizando UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,7 +4,6 @@ import { v4 } from 'uuid'
 class UserController {
     async store(req, res) {
         const { name, email, password_hash, admin } = req.body
-
         const user = await User.create({
             id: v4(),
             name,
