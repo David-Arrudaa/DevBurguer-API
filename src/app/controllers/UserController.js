3824701681bdd18aa78988447bf9584257555851
David_Arruda <david.viniciusarruda@gmail.com>
2025-09-21 21:40:53 -0300

    Validando dados com Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,21 +1,38 @@
+import { request } from 'express'
 import User from '../models/User'
 import { v4 } from 'uuid'
+import * as Yup from "yup"
+
+
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        })
+
+        try {
+            schema.validateSync(req.body, { abortEarly: false});
+        } catch (err) {
+            return res.status(400).json({ error: err.errors });
+        }
+
         const { name, email, password_hash, admin } = req.body
         const user = await User.create({
             id: v4(),
             name,
             email,
             password_hash,
-            admin
+            admin,
         })
         return res.status(201).json({
             id: user.id,
             name,
             email,
-            admin
+            admin,
         });
     }
 }
