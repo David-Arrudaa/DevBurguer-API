5989a7a5e882b95b53b9ffac690ab1938099715b
David_Arruda <david.viniciusarruda@gmail.com>
2025-09-21 21:52:48 -0300

    Validando email duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,12 +15,23 @@ class UserController {
         })
 
         try {
-            schema.validateSync(req.body, { abortEarly: false});
+            schema.validateSync(req.body, { abortEarly: false });
         } catch (err) {
             return res.status(400).json({ error: err.errors });
         }
 
         const { name, email, password_hash, admin } = req.body
+
+        const userExists = await User.findOne({
+            where: {
+                email,
+            }
+        })
+
+        if (userExists) {
+            return res.status(400).json({ error : "User already exists"})
+        }
+
         const user = await User.create({
             id: v4(),
             name,
